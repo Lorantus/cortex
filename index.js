b51182a639b8258ca29fe5547fa15b6e7b6dc7cc
Laurent <vernaz.laurent@gmail.com>
2018-08-19 16:07:42 +0200

    Mise à jour des podcast avec leur numéro

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ import Context from './CortexContext';
 
 const commander = {};
 
+// 6656918  Cortex
+// 9673679  Raconte-moi-une-histoire-vraie
+const idPodcast = 9673679;
+
 Context.buildContext({
-    url: 'https://www.rts.ch/play/radio/show/6656918/latestEpisodes', 
+    url: `https://www.rts.ch/play/radio/show/${idPodcast}/latestEpisodes`, 
     destinationFolder: commander.dest ? commander.dest : './dest'
 }).extractDownloadInformations(commander.pages);
